ec585ac1d1e2493fb739de4c03c30e9aa4cd8679
aliquazaimodo <ajali4487@gmail.com>
2022-11-10 23:19:46 +0330

    for.py
    this is a practice  and it isn.t true

---
+++ f.py
@@ -0,0 +1,7 @@
+count = 0
+myfamily = ('hassan','ali','naghi','assad','mehdi')
+for thisone in myfamily:
+    print('you are useless',thisone)
+    count = count+1
+    print('i said',count,'you are useless')
+    
